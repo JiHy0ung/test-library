47f800383760553d19b3f14e256f435785653548
JiHyoung You <fbwlgud51@gmail.com>
2025-04-17 22:13:00 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,14 @@ import PrivateRoute from "./route/PrivateRoute";
 import loginStore from "./stores/loginStore";
 
 function App() {
-  
-  const {authenticate, login, logout} = loginStore();
-
   return (
     <div className="full-container">
       <Routes>
-        <Route path="/" element={<AppLayout authenticate={authenticate} login={login} />}>
+        <Route path="/" element={<AppLayout />}>
           <Route index element={<HomePage />} />
 
-          <Route path="/login" element={<Login login={login}/>} />
-          <Route path="/mybook" element={<PrivateRoute authenticate={authenticate}/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/mybook" element={<PrivateRoute />} />
         </Route>
       </Routes>
     </div>
--- src/route/PrivateRoute.jsx
+++ src/route/PrivateRoute.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import MyBookPage from '../pages/MyBookPage/MyBookPage';
+import loginStore from '../stores/loginStore';
 
-const PrivateRoute = ({authenticate}) => {
+const PrivateRoute = () => {
+
+    const {authenticate} = loginStore();
     return (
         authenticate == true ? <MyBookPage/> : <Navigate to="/login"/>
       );
